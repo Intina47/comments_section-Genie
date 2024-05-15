22bf0c9d1ddd204e569a41a0737a0ae105bc6210
mamba <isaiahntina47@gmail.com>
2024-05-15 20:36:11 +0000

    chore: Add Firebase configuration files

---
+++ comments.js
@@ -0,0 +1,12 @@
+[
+    'Best asmrtist on the planet. Your clicky whispers are beyond S tier! I would love it if you made a whole video of you saying “really”. Always incredibly tingly.',
+    'I have replayed this probably 15 times by now. So nice. Thank you ❤',
+    'Thank goodness this popped up in my feed!! I missed it in my subscribers section 😢 perfect to send me to sleep tonight 🎉',
+    'your asmr is so goooooooddd, I love it 💗💗🇧🇷🇧🇷',
+    'It&#39;s time to sleep 😴 Thank you my dear for the video I love it asmr your You are really creative ❤️‍🩹',
+    'BEST VIDEO I HAVR EVER WATCHHHEDD AND IVE BEEN WATCJNG ASMR SINCE 2018 THANK U SOO MICH HONESTLY UR CHANNEL IS AMAZINGG AND ALSO IM LEARNING ABIT OF GERMAN SEHRRR FANTASTISCH!! DANKE',
+    'Perfect ❤',
+    'You&#39;re the best. It&#39;s amazing how your asmrs make me sleepy so quickly 😍',
+    'genuinely found my new fave asmrtist!! i love the non-aggressive nature of your asmr it’s so calming',
+    'I wish plucking could be added ! I love plucking fluffy mic but there were not much clips about this haha'
+  ]
