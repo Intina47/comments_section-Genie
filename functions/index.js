3485e19a3eb96785acbcbfa045d372909c8dd28d
mamba <isaiahntina47@gmail.com>
2024-05-15 22:51:13 +0000

    chore: Add sentiment and question analysis to YouTube comments retrieval

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,10 @@
 const functions = require("firebase-functions");
+const client = require("@google-cloud/language");
 const axios = require("axios");
 const dotenv = require("dotenv");
 const he = require("he");
 dotenv.config();
-
+const languageClient = new client.LanguageServiceClient();
 
 /**
  * Retrieves comments for a YouTube video using the YouTube Data API.
@@ -19,14 +20,19 @@ async function getComments(videoId, apiKey) {
         part: "snippet",
         videoId: videoId,
         key: apiKey,
-        maxResults: 10, // You can adjust this value
+        maxResults: 10, 
       },
     });
 
-    const comments = response.data.items.map(
-        (item) =>
-          preprocessComment(item.snippet.topLevelComment.snippet.textDisplay));
-    console.log(comments);
+    const comments = await Promise.all(response.data.items.map(async (item) => {
+      const comment = preprocessComment(
+          item.snippet.topLevelComment.snippet.textDisplay,
+      );
+      const sentiment = await analyzeSentiment(comment);
+      const isQuestion = await analyzeSyntax(comment);
+      return {comment, sentiment, isQuestion};
+    }));
+
     return comments;
   } catch (error) {
     console.error(error);
@@ -61,11 +67,12 @@ exports.getVideoComments = functions.https.onRequest(
  * @return {string} The preprocessed comment.
  */
 function preprocessComment(comment) {
+  // Remove comments with URLs
+  if (comment.match(/https?:\/\/\S+/)) {
+    return "";
+  }
   // HTML decode
   comment = he.decode(comment);
-
-  // Replace URLs with 'LINK'
-  comment = comment.replace(/https?:\/\/[^\s]+/g, "LINK");
 
   // Convert to lowercase
   comment = comment.toLowerCase();
@@ -77,4 +84,41 @@ function preprocessComment(comment) {
   comment = comment.trim();
 
   return comment;
+}
+
+/**
+ * Analyzes the sentiment of the given text.
+ * @param {string} text - The text to analyze.
+ * @return {object} - The sentiment analysis result.
+ */
+async function analyzeSentiment(text) {
+  const document = {
+    content: text,
+    type: "PLAIN_TEXT",
+  };
+  const [result] = await languageClient.analyzeSentiment({document: document});
+  const sentiment = result.documentSentiment;
+
+  return sentiment;
+}
+
+/**
+ * Analyzes the syntax of the given text and determines
+ * if it contains a question.
+ *
+ * @param {string} text - The text to analyze.
+ * @return {boolean} - True if the text contains a question, false otherwise.
+ */
+async function analyzeSyntax(text) {
+  const document = {
+    content: text,
+    type: "PLAIN_TEXT",
+  };
+
+  const [syntax] = await languageClient.analyzeSyntax({document});
+  const tokens = syntax.tokens;
+
+  const isQuestion = tokens.some((token) => token.text.content === "?");
+
+  return isQuestion;
 }
